7497f6eae645bdf15cef9b8075b4e6a80ae7952c
MartinBINARD <martin.binard.dev@gmail.com>
2023-11-27 19:18:02 +0100

    bugfix home-page : restore image and logo in link

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,40 +1,34 @@
 import { Link } from 'react-router-dom';
+import { Compass } from 'lucide-react';
 
 // import of picture and icons for the homepage
-import logo from '../../assets/logo/compass.png';
+import illustration from '../../assets/images/illustration.png';
 import Footer from '../../components/Footer/Footer';
 
 function Homepage() {
   return (
     <>
-      <div className="lg:h-screen w-screen absolute">
-        {/* <img
-          className="absolute bottom-0 object-fill w-full "
-          src={Bg}
-          alt="background illustration"
-        /> */}
-      </div>
       <div className="flex flex-col justify-center z-10 items-center lg:flex-row m-auto lg:gap-20">
-        {/* <img
+        <img
           className="max-w-xs m-auto z-10 lg:m-5 shrink"
-          src={koala}
-          alt="illustration d'accueil"
-        /> */}
+          src={illustration}
+          alt="illustration de l'application"
+        />
         <section className="flex flex-col gap-10 w-3/4 lg:w-1/3 text-primary">
           <h3 className="font-bold text-4xl">
             Découvre le monde sous un
-            <span className="text-accent">nouvel angle</span>
+            <span className="text-accent ml-2">nouvel angle</span>
           </h3>
           <p className="text-xl">
             Rejoins la communauté des explorateurs de
-            <span className="font-bold italic">CachTrek</span>, parcours des
-            endroits étonnants et apprend tout en t&apos;amusant !
+            <span className="font-bold italic ml-2">CachTrek</span>, parcours
+            des endroits étonnants et apprend tout en t&apos;amusant !
           </p>
           <Link
-            className="flex w-max items-center gap-2 border rounded-md btn btn-primary btn-lg normal-case text-xl font-medium text-white"
+            className="flex w-max items-center gap-2 border rounded-md btn btn-primary btn-lg normal-case text-2xl font-medium text-white"
             to="/presentation"
           >
-            <img className="h-10" src={logo} alt="Logo CacheTrek" />
+            <Compass size={34} />
             Découvrir
           </Link>
         </section>
