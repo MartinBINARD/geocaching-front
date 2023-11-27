c8f8de8d244737259410fc26b0a9de9db9d9ea2e
MartinBINARD <martin.binard.dev@gmail.com>
2023-11-27 19:28:13 +0100

    error-page : convert to tsx and fix logo

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-import logo from '../../assets/logo/compass.png';
+import { Compass } from 'lucide-react';
 
 function ErrorPage() {
   return (
@@ -11,11 +11,11 @@ function ErrorPage() {
       <p className="mt-5 text-lg">Cette page n&apos;existe pas</p>
 
       <Link
-        className="flex items-center gap-2 border rounded-md py-1 px-3 border-primary bg-primary text-white text-sm m-2 sm:text-2xl sm:w-max"
+        className="flex items-center gap-2 border rounded-md p-4 border-primary bg-primary text-white text-sm m-2 sm:text-2xl sm:w-max"
         to="/"
       >
         Retourner vers la page d&apos;accueil
-        <img className="h-10" src={logo} alt="logo CacheTrek" />
+        <Compass size={34} />
       </Link>
     </div>
   );
