6ef634c73ee1ff0bc37f19ea290add7f542c46ec
MartinBINARD <martin.binard.dev@gmail.com>
2023-11-29 15:50:40 +0100

    typescript-footer : convert to tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,5 @@
 import { Link } from 'react-router-dom';
 
-// Building a footer componant with Link to privacy policy
-
 function Footer() {
   return (
     <footer className="p-4 flex z-10 mb-0 mt-10 gap-3 text-xs lg:text-sm lg:justify-between lg:bg-[#00453E] footer lg:text-white text-primary">
