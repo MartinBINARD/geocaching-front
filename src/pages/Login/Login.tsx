01becfe4368431cb836220f82b025a57c9c374e4
MartinBINARD <martin.binard.dev@gmail.com>
2023-12-05 14:27:51 +0100

    typescript_login_register : refactor ternary operator in component in login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -18,18 +18,15 @@ function Login() {
   );
   const loading = useAppSelector((state) => state.settings.loading);
 
-  // Form login processing
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
     const form = e.target as HTMLFormElement;
     const formData = new FormData(form) as unknown as HTMLFormElement;
 
-    // Making the email to lower case before checking at database
     const emailInput = formData.get('email') as string;
     const emailToLowerCase = emailInput.toLowerCase();
     formData.set('email', emailToLowerCase);
 
-    // Using dispatch to send formData to Login POST
     dispatch(login(formData));
   };
 
@@ -43,9 +40,9 @@ function Login() {
         <MountainSnow className="w-44 h-44 m-5" />
         <div className="lg:w-1/3 flex flex-col items-center lg:items-start lg:border-l lg:border-primary lg:pl-20">
           <h2 className="font-bold text-xl my-5">Connexion</h2>
-          {failedLogin ? (
+          {failedLogin && (
             <h4 className="text-red-500 font-semibold">{errorMessage}</h4>
-          ) : null}
+          )}
           <form
             className="form-control w-full max-w-xs"
             onSubmit={handleSubmit}
@@ -88,7 +85,7 @@ function Login() {
             >
               Pas encore inscrit ?
             </Link>
-            {user ? <Navigate to="/profile" /> : null}
+            {user && <Navigate to="/profile" />}
           </div>
         </div>
       </div>
