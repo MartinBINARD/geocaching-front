13231225044adbd90e36db3af71a47cdbe44483b
MartinBINARD <martin.binard.dev@gmail.com>
2023-12-05 16:26:30 +0100

    typescript_login_register : Refactor condtion to verified user

--- src/pages/Verify/Verify.tsx
+++ src/pages/Verify/Verify.tsx
@@ -19,19 +19,18 @@ function Verify() {
     }
   }, [dispatch, token]);
 
-  // If the call is rejected
-  if (!verified) {
-    return (
-      <section className="flex flex-col justify-center items-center m-auto lg:shadow-lg p-4">
-        <h2 className="font-bold">La vérification à échoué !</h2>
-        <p className="p-2">
-          Assurez-vous de n&apos;avoir pas déjà vérifié ce compte !
-        </p>
-      </section>
-    );
+  if (verified) {
+    return <Navigate to="/login" />;
   }
 
-  return verified && <Navigate to="/login" />;
+  return (
+    <section className="flex flex-col justify-center items-center m-auto lg:shadow-lg p-4">
+      <h2 className="font-bold">La vérification à échoué !</h2>
+      <p className="p-2">
+        Assurez-vous de n&apos;avoir pas déjà vérifié ce compte !
+      </p>
+    </section>
+  );
 }
 
 export default Verify;
