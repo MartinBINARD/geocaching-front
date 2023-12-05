2b300a750decd7ef449c6c73259ba19651d5519b
MartinBINARD <martin.binard.dev@gmail.com>
2023-12-05 15:53:28 +0100

    typescript_login_register : convert verify page to tsx and type token varaible

--- src/pages/Verify/Verify.tsx
+++ src/pages/Verify/Verify.tsx
@@ -1,31 +1,31 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-// import verufy from settings' reducer
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+
 import { verify } from '../../store/reducers/settings';
 
 function Verify() {
   // Init dispatch and location and use URLSearchParams to get the token
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const token = searchParams.get('token');
+  const token: string | null = searchParams.get('token');
 
-  // Get the verified state from the reducer
-  const verified = useSelector((state) => state.settings.verified);
+  const verified = useAppSelector((state) => state.settings.verified);
 
-  // Call API to verify
   useEffect(() => {
-    dispatch(verify(token));
+    if (token) {
+      dispatch(verify(token));
+    }
   }, [dispatch, token]);
 
   // If the call is rejected
   if (!verified) {
     return (
       <section className="flex flex-col justify-center items-center m-auto lg:shadow-lg p-4">
-        <h2 className="font-bold">La vérification à échoué !</h2>{' '}
+        <h2 className="font-bold">La vérification à échoué !</h2>
         <p className="p-2">
-          Assurez-vous de n&apos;avoir pas déjà vérifié ce compte !{' '}
+          Assurez-vous de n&apos;avoir pas déjà vérifié ce compte !
         </p>
       </section>
     );
