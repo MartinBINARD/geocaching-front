f52c792acdce4f6f452b17f8adca58f8bfc32782
MartinBINARD <martin.binard.dev@gmail.com>
2023-12-19 11:34:56 +0100

    typescript_profile: Convert TextArea component into tsx

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,11 +1,17 @@
-function TextArea({ keyName, label, placeholder, defaultValue }) {
+interface TextAreaProps {
+  name: string;
+  label: string;
+  placeholder: string;
+  defaultValue: string | null;
+}
+function TextArea({ name, label, placeholder, defaultValue }: TextAreaProps) {
   return (
     <div className="form-control my-1">
       <label className="label">
         <span className="label-text">{label}</span>
       </label>
       <textarea
-        name={keyName}
+        name={name}
         defaultValue={defaultValue || ''}
         placeholder={placeholder}
         className="textarea textarea-bordered h-24 w-full"
