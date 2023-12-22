5e44d9a7bda852a9c1d5c439a24b12b109c05d6b
MartinBINARD <martin.binard.dev@gmail.com>
2023-12-22 14:21:35 +0100

    typescript_one_circuit_path : Remove broken components and add new icons

--- src/pages/Circuit/Circuit.jsx
+++ src/pages/Circuit/Circuit.jsx
@@ -1,47 +1,36 @@
-/* eslint-disable react/button-has-type */
-/* eslint-disable react/prop-types */
-
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, Link, Navigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import {
+  Brain,
+  Compass,
+  DraftingCompass,
+  Footprints,
+  Hourglass,
+  MapPinned,
+  MountainSnow,
+} from 'lucide-react';
 
-// import fetchCircuit from circuits' reducer
 import { fetchCircuit } from '../../store/reducers/circuits';
 
-// import components
-import MapCircuit from '../../components/MapCircuit/MapCircuit';
+// import MapCircuit from '../../components/MapCircuit/MapCircuit';
 import Loader from '../../components/Loader/Loader';
 
-import logo from '../../assets/logo/compass.png';
-
 function Circuit() {
-  // state to listening if the user want to see the map
   const [showMap, setShowMap] = useState(false);
-
-  // state to stock difficulty of the circuit and display the good icon
-  const [starDifficulty, setStarDifficulty] = useState(null);
-
-  // state to stock terrain level of the circuit and display the good icon
-  const [starTerrain, setStarTerrain] = useState(null);
 
   /* state to know if the fetch API is already load. 
   Init it with false, fill it with noCircuit reducer state when we got the circuit
   The first goal is to prevent redirecting while we don't get API answer */
   const [alreadyLoad, setAlreadyLoad] = useState(false);
 
-  // init dispatch
-  const dispatch = useDispatch();
-
-  // Getting the id with URL
   const { id } = useParams();
-  // state to know if the call API is pending
   const loading = useSelector((state) => state.circuits.loading);
-  // state to know if a circuit with this id exist
   const noCircuit = useSelector((state) => state.circuits.noCircuit);
-  // state to know if there is a user (with user informations)
   const user = useSelector((state) => state.settings.user);
-  // state to get the circuit call
   const circuit = useSelector((state) => state.circuits.oneCircuit);
+
+  const dispatch = useDispatch();
 
   // useEffect hook to get the right circuit
   useEffect(() => {
@@ -49,51 +38,11 @@ function Circuit() {
   }, [dispatch, id]);
 
   // useEffect hook to set the right star icon for difficulty and terrain
-  // useEffect(() => {
-  //   if (circuit) {
-  //     switch (circuit.difficulty) {
-  //       case 1:
-  //         setStarDifficulty(starone);
-  //         break;
-  //       case 2:
-  //         setStarDifficulty(startwo);
-  //         break;
-  //       case 3:
-  //         setStarDifficulty(starthree);
-  //         break;
-  //       case 4:
-  //         setStarDifficulty(starfour);
-  //         break;
-  //       case 5:
-  //         setStarDifficulty(starfive);
-  //         break;
-  //       default:
-  //         setStarDifficulty(starfive);
-  //     }
-
-  //     switch (circuit.terrain) {
-  //       case 1:
-  //         setStarTerrain(starone);
-  //         break;
-  //       case 2:
-  //         setStarTerrain(startwo);
-  //         break;
-  //       case 3:
-  //         setStarTerrain(starthree);
-  //         break;
-  //       case 4:
-  //         setStarTerrain(starfour);
-  //         break;
-  //       case 5:
-  //         setStarTerrain(starfive);
-  //         break;
-  //       default:
-  //         setStarTerrain(starfive);
-  //     }
-
-  //     setAlreadyLoad(noCircuit);
-  //   }
-  // }, [circuit, noCircuit]);
+  useEffect(() => {
+    if (circuit) {
+      setAlreadyLoad(noCircuit);
+    }
+  }, [circuit, noCircuit]);
 
   //  UseEffect hook to init local storage
   useEffect(() => {
@@ -128,6 +77,7 @@ function Circuit() {
           src={circuit.url_image}
           alt={`circuit : ${circuit.name}`}
         />
+
         <section className="border-b border-primary m-auto">
           <p className="text-center m-auto pt-3">
             {circuit.region}, {circuit.state} - {circuit.city}
@@ -136,39 +86,55 @@ function Circuit() {
             {circuit.theme}
           </p>
         </section>
+
         <section className="flex justify-between item-center p-4 shadow-lg rounded-lg">
-          <div className="flex flex-col gap-1">
-            <img
-              className="h-8"
-              src={starDifficulty}
-              alt={`niveau de difficulté : ${circuit.difficulty}/5`}
+          <div className="flex flex-col items-center gap-1">
+            <Brain
+              className="w-8 h-8 text-secondary"
+              alt="niveau de difficulté intellectuelle"
             />
-            <p>Difficulté</p>
+            <p>
+              Difficulté :
+              <span className="font-bold ml-1">{circuit.difficulty}/5</span>
+            </p>
           </div>
-          <div className="flex flex-col gap-1">
-            <img
-              className="h-8"
-              src={starTerrain}
-              alt={`niveau de terrain : ${circuit.terrain}/5`}
+          <div className="flex flex-col items-center gap-1">
+            <MountainSnow
+              className="w-8 h-8 text-secondary"
+              alt="niveau de difficulté intellectuelle"
             />
-            <p>Terrain</p>
-          </div>
-          <div className="flex flex-col gap-1">
-            <p className="h-8 text-center text-base font-bold">
-              {circuit.distance} km
+            <p>
+              Terrain :
+              <span className="font-bold ml-1">{circuit.terrain}/5</span>
             </p>
-            <p>Distance</p>
           </div>
-          <div className="flex flex-col gap-1">
-            <p className="h-8 text-center text-base font-bold">
-              {circuit.duration}
+          <div className="flex flex-col items-center gap-1">
+            <DraftingCompass
+              className="w-8 h-8 text-secondary"
+              alt="distance kilométrique"
+            />
+            <p>
+              Distance :
+              <span className="font-bold ml-1">{circuit.distance} km</span>
             </p>
-            <p>Durée</p>
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <Hourglass
+              className="w-8 h-8 text-secondary"
+              alt="estimation de la durée"
+            />
+            <p>
+              Durée :<span className="font-bold ml-1">{circuit.duration}</span>
+            </p>
           </div>
         </section>
-        <div className="flex flex-wrap justify-center items-center gap-3 m-auto mt-5 p-2">
+
+        <section className="flex flex-wrap justify-center items-center gap-3 m-auto mt-5 p-2">
           <div className="flex items-center gap-2">
-            <img className="h-10" src={shoes} alt="icone de chaussure" />
+            <Footprints
+              className="w-24 h-24 text-secondary"
+              alt="icone de chaussure"
+            />
             <p>Mobilité :</p>
           </div>
           <p>
@@ -180,27 +146,34 @@ function Circuit() {
               </span>
             ))}
           </p>
-        </div>
+        </section>
+
         <p className="p-2 m-auto">{circuit.introduction}</p>
-        <div className="flex items-center gap-5 lg:gap-10 p-2 px-4 border-t border-b border-primary m-auto lg:w-3/4 lg:justify-center">
+
+        <section className="flex items-center gap-5 lg:gap-10 p-2 px-4 border-t border-b border-primary m-auto lg:w-3/4 lg:justify-center">
           <div className="flex flex-col">
             <p>Parking :</p>
             <p>{circuit.parking_address}</p>
           </div>
           <button
-            className="flex flex-col text-sm"
             type="button"
+            className="btn btn-outline btn-primary flex flex-col items-center text-sm"
             onClick={handleClickMap}
           >
-            {/* <img className="h-12" src={pins} alt="Pins koala" /> */}
+            <MapPinned
+              className="w-8 h-8"
+              alt="position du circuit sur la carte"
+            />
             Voir le plan
           </button>
-        </div>
+        </section>
+
         {showMap && (
           <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50">
             <div className="relative w-4/5 h-4/5 bg-white">
               <button
-                className="absolute top-full left-1/2 transform -translate-x-1/2 mt-4 text-white bg-red-500 rounded-full w-8 h-8 flex items-center justify-center hover:text-red-600 focus:outline-none"
+                type="button"
+                className="absolute top-full left-1/2 transform -translate-x-1/2 mt-4"
                 onClick={handleClickMap}
               >
                 X
@@ -213,12 +186,13 @@ function Circuit() {
             </div>
           </div>
         )}
+
         {user ? (
           <Link
             className="flex gap-2 items-center  mt-2 btn btn-primary text-white m-auto"
             to={`/circuit/${id}/map`}
           >
-            <img className="h-10" src={logo} alt="Logo CacheTrek" /> Commencer
+            <Compass className="w-7 h-7" alt="Logo CacheTrek" /> Commencer
           </Link>
         ) : (
           <Link
