e5a5149dc93967c566f156db4f7930eeaa7ebbb7
MartinBINARD <martin.binard.dev@gmail.com>
2024-01-12 22:29:00 +0100

    feature/lucid-icons : convert svg in lucid-icons in FilterCircuit component

--- src/components/FilterCircuitsLists/FilterCircuitsList.tsx
+++ src/components/FilterCircuitsLists/FilterCircuitsList.tsx
@@ -1,16 +1,19 @@
-/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import { useCallback, useState } from 'react';
+import {
+  ChevronDown,
+  ChevronUp,
+  Search,
+  SlidersHorizontal,
+} from 'lucide-react';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+
+import { Circuit } from '../../@types/circuit';
+
 import { searchCircuitsList } from '../../store/reducers/circuits';
 
 import SelectControl from '../SelectControl/SelectControl';
 import Loader from '../../loader/Loader';
-
-import settings from '../../assets/icon/vertical_settings.svg';
-import chevronUp from '../../assets/icon/chevron-up.svg';
-import chevronDown from '../../assets/icon/chevron-down.svg';
-
-import { Circuit } from '../../@types/circuit';
 
 interface ListProps {
   list: Circuit[];
@@ -66,14 +69,14 @@ function FilterCircuitsList({ list }: ListProps) {
             className="flex flex-row flex-nowrap items-center justify-between cursor-pointer w-full lg:hidden"
           >
             <div className="flex flex-row flex-nowrap">
-              <img className="h-5" src={settings} alt="filter menu setting" />
+              <SlidersHorizontal className="w-5 h-5 text-primary" />
               <p className="ml-4">Filtres</p>
             </div>
-            <img
-              className="h5"
-              src={isOpen ? chevronDown : chevronUp}
-              alt="filter menu chevron"
-            />
+            {isOpen ? (
+              <ChevronDown className="w-5 h-5 text-primary" />
+            ) : (
+              <ChevronUp className="w-5 h-5 text-primary" />
+            )}
           </button>
 
           <div
@@ -152,20 +155,7 @@ function FilterCircuitsList({ list }: ListProps) {
             </ul>
 
             <button type="submit" className="btn btn-ghost btn-circle mt-9">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
+              <Search className="w-5 h-5 text-primary" />
             </button>
           </div>
         </div>
