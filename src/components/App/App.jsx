5168afaa0cb94da5a9913136b68eb98171140ddb
MartinBINARD <martin.binard.dev@gmail.com>
2024-01-12 21:52:39 +0100

    refactor/remove-admin : Remove App.css

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,11 @@
-// import of Outlet from react-router-dom that will be remplace by pages in router.jsx
 import { Outlet } from 'react-router-dom';
 
-// import of components that will stay in every pages of our site
 import Header from '../Header/Header';
 import Toast from '../Toast/Toast';
 
-import './App.css';
-
-// create an app component that will be the first structure of our site
 function App() {
   return (
-    <div className="App font-body">
+    <div className="flex flex-col min-h-screen text-primary bg-white font-body">
       <Header />
       <Toast />
       <Outlet />
