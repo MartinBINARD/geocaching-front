00936d6f64fce875d1bca3de33af9a887377cb02
MartinBINARD <martin.binard.dev@gmail.com>
2024-01-18 11:28:51 +0100

    feature/new-resposinve-map : Move comments to top in CircuitsList page

--- src/pages/CircuitsList/CircuitsList.tsx
+++ src/pages/CircuitsList/CircuitsList.tsx
@@ -8,17 +8,19 @@ import Loader from '../../loader/Loader';
 import FilterCircuitsList from '../../components/FilterCircuitsLists/FilterCircuitsList';
 import CircuitListCard from '../../components/CircuitsListCard/CircuitsListCard';
 
+/* First load all circuitsList by default or when empty search value is
+submitted. Replace all circuitsList prop by searchList when updating from store
+/ submitting from FilterCircuitsList component */
+
+/* If no search result, so show warning message but show all markers on map and
+list because center point calculated with markers coordonates is required
+to build map */
+
 function CircuitsList() {
   const errorMessage = useAppSelector((state) => state.circuits.errorMessage);
   const isLoading = useAppSelector((state) => state.circuits.isLoading);
-  /* First load all circuitsList by default or when empty search value is
-  submitted. Replace all circuitsList prop by searchList when updating from store
-  / submitting from FilterCircuitsList component */
   const circuitsList = useAppSelector((state) => state.circuits.circuitsList);
   const searchList = useAppSelector((state) => state.circuits.searchList);
-  /* If no search result, so show warning message but show all markers on map and
-  list because center point calculated with markers coordonates is required
-  to build map */
 
   const list = searchList.length ? searchList : circuitsList;
 
