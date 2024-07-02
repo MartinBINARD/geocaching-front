1844c2ddf0d6eac58b1ddeeb458007a6e3a6dd20
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-01 14:22:07 -1000

    Add updatePasswordUseCase.test in __tests__/usecases/auth/

---
+++ src/__tests__/usecases/auth/updatePasswordUseCase.test.ts
@@ -0,0 +1,67 @@
+import { AxiosError } from 'axios';
+import {
+  updateCredentialsEntries,
+  updateCredentialsErrorResponse,
+} from '../../../__mocks__/auth.mocks';
+import { updatePassword } from '../../../domain';
+import { UpdateCredentials } from '../../../domain/entities/auth';
+import authReducer, { intialAuthState } from '../../../domain/usecases/auth';
+
+const fakeRequestId = 'fakeRequestId';
+
+jest.mock('../../../services/axios', () => ({
+  api: {
+    baseUrl: 'http://localhost:3000',
+  },
+}));
+
+describe('Authentication store', () => {
+  it('Should return the initial auth state on first call', () => {
+    expect(authReducer(undefined, { type: '@@INIT' })).toBe(intialAuthState);
+  });
+});
+
+describe('Update password state test', () => {
+  it('Should SUCCEED to update password', () => {
+    const fakeEntries =
+      updateCredentialsEntries as unknown as UpdateCredentials;
+    const fakePayload = true;
+
+    const action = updatePassword.fulfilled(
+      fakePayload,
+      fakeRequestId,
+      fakeEntries
+    );
+    const state = authReducer(intialAuthState, action);
+
+    expect(action.type).toEqual('settings/update-password/fulfilled');
+    expect(action.payload).toBeTruthy;
+    expect(action.meta.requestId).toEqual(fakeRequestId);
+    expect(action.meta.arg).toEqual(fakeEntries);
+
+    expect(state).toEqual({ ...intialAuthState, isRegistered: true });
+    expect(state.isLoading).toBeFalsy;
+  });
+
+  it('Should FAIL to update password', () => {
+    const fakeEntries =
+      updateCredentialsEntries as unknown as UpdateCredentials;
+    const fakePayload = updateCredentialsErrorResponse as AxiosError;
+
+    const action = updatePassword.rejected(
+      fakePayload,
+      fakeRequestId,
+      fakeEntries
+    );
+    const state = authReducer(intialAuthState, action);
+
+    expect(action.type).toEqual('settings/update-password/rejected');
+    expect(action.payload).toEqual(undefined);
+    expect(action.meta.requestId).toEqual(fakeRequestId);
+    expect(action.meta.arg).toEqual(fakeEntries);
+    expect(action.error).toEqual(fakePayload);
+
+    expect(state).toEqual({ ...intialAuthState, isRegistered: false });
+    expect(state.isLoading).toBeFalsy;
+  });
+});
