78f34809f5dc4c07ad2ca21d0a4d8ae1c5b97943
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-03 15:54:55 -1000

    Add test to updatePrfoile usecase

--- src/__tests__/usecases/user.test.ts
+++ src/__tests__/usecases/user.test.ts
@@ -1,15 +1,8 @@
-import { AxiosError } from 'axios';
-import { Profile, UpdateProfileForm } from '../../domain/entities/user';
 import userReducer, {
   deleteProfile,
   intialUserState,
-  updateProfile,
 } from '../../domain/usecases/user';
-import {
-  userProfileFormEntries,
-  userProfileFormErrorResponse,
-  userProfileResponse,
-} from '../../__mocks__/user.mocks';
+import { userProfileResponse } from '../../__mocks__/user.mocks';
 
 const fakeRequestId = 'fakeRequestId';
 
@@ -22,53 +15,6 @@ jest.mock('../../services/axios', () => ({
 describe('User store', () => {
   it('Should return the initial user state on first call', () => {
     expect(userReducer(undefined, { type: '@@INIT' })).toBe(intialUserState);
-  });
-});
-
-describe('UPDATE user profile state test', () => {
-  it('Should SUCCEED to update user profile', () => {
-    const fakeEntries = userProfileFormEntries as unknown as UpdateProfileForm;
-    const fakePayload: Profile = userProfileResponse;
-
-    const action = updateProfile.fulfilled(
-      fakePayload,
-      fakeRequestId,
-      fakeEntries
-    );
-    const state = userReducer(intialUserState, action);
-
-    expect(action.type).toEqual('user/update-profile/fulfilled');
-    expect(action.payload).toEqual(fakePayload);
-    expect(action.meta.requestId).toEqual(fakeRequestId);
-    expect(action.meta.arg).toEqual(fakeEntries);
-
-    expect(state).toEqual({ ...intialUserState, profile: fakePayload });
-    expect(state.isProfileLoading).toBeFalsy;
-  });
-
-  it('Should FAIL to update user profile', () => {
-    const fakeEntries = userProfileFormEntries as unknown as UpdateProfileForm;
-    const fakePayload = userProfileFormErrorResponse as AxiosError;
-
-    const action = updateProfile.rejected(
-      fakePayload,
-      fakeRequestId,
-      fakeEntries
-    );
-    const state = userReducer(intialUserState, action);
-
-    expect(action.type).toEqual('user/update-profile/rejected');
-    expect(action.payload).toEqual(undefined);
-    expect(action.meta.requestId).toEqual(fakeRequestId);
-    expect(action.meta.arg).toEqual(fakeEntries);
-    expect(action.error).toEqual(fakePayload);
-
-    expect(state).toEqual({
-      ...intialUserState,
-      errorMessage: fakePayload.message,
-    });
-    expect(state.profile).toBeNull;
-    expect(state.isProfileLoading).toBeFalsy;
   });
 });
 
