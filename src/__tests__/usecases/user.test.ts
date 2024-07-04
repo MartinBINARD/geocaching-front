5ab4ceb6ec7013f9bf012ce8fc33e6345255244e
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-03 15:46:18 -1000

    Add tests to getProfileUseCase

--- src/__tests__/usecases/user.test.ts
+++ src/__tests__/usecases/user.test.ts
@@ -2,7 +2,6 @@ import { AxiosError } from 'axios';
 import { Profile, UpdateProfileForm } from '../../domain/entities/user';
 import userReducer, {
   deleteProfile,
-  getProfile,
   intialUserState,
   updateProfile,
 } from '../../domain/usecases/user';
@@ -23,44 +22,6 @@ jest.mock('../../services/axios', () => ({
 describe('User store', () => {
   it('Should return the initial user state on first call', () => {
     expect(userReducer(undefined, { type: '@@INIT' })).toBe(intialUserState);
-  });
-});
-
-describe('GET user profile state test', () => {
-  it('Should SUCCEED to GET user profile', () => {
-    const fakePayload: Profile = userProfileResponse;
-
-    const action = getProfile.fulfilled(fakePayload, fakeRequestId);
-    const state = userReducer(intialUserState, action);
-
-    expect(action.type).toEqual('user/get-profile/fulfilled');
-    expect(action.payload).toEqual(fakePayload);
-    expect(action.meta.requestId).toEqual(fakeRequestId);
-    expect(action.meta.arg).toEqual(undefined);
-
-    expect(state).toEqual({ ...intialUserState, profile: fakePayload });
-    expect(state.isProfileLoading).toBeFalsy;
-    expect(state.errorMessage).toBeNull;
-  });
-
-  it('Should FAIL to GET user profile', () => {
-    const fakePayload = null;
-
-    const action = getProfile.rejected(fakePayload, fakeRequestId);
-    const state = userReducer(intialUserState, action);
-
-    expect(action.type).toEqual('user/get-profile/rejected');
-    expect(action.payload).toEqual(undefined);
-    expect(action.meta.requestId).toEqual(fakeRequestId);
-    expect(action.meta.arg).toEqual(undefined);
-    expect(action.error).toEqual({ message: 'Rejected' });
-
-    expect(state).toEqual({
-      ...intialUserState,
-      profile: null,
-      errorMessage: 'Rejected',
-    });
-    expect(state.isProfileLoading).toBeFalsy;
   });
 });
 
---
+++ src/__tests__/usecases/user/getProfileUseCase.test.ts
@@ -0,0 +1,55 @@
+import { fakeRequestId } from '../../../__mocks__/request.mocks';
+import { userProfileResponse } from '../../../__mocks__/user.mocks';
+import userReducer, { intialUserState } from '../../../domain/usecases/user';
+import { getProfile } from '../../../domain';
+import { Profile } from '../../../domain/entities/user';
+
+jest.mock('../../../services/axios', () => ({
+  api: {
+    baseUrl: 'http://localhost:3000',
+  },
+}));
+
+describe('User store', () => {
+  it('Should return the initial user state on first call', () => {
+    expect(userReducer(undefined, { type: '@@INIT' })).toBe(intialUserState);
+  });
+});
+
+describe('GET user profile state test', () => {
+  it('Should SUCCEED to GET user profile', () => {
+    const fakePayload: Profile = userProfileResponse;
+
+    const action = getProfile.fulfilled(fakePayload, fakeRequestId);
+    const state = userReducer(intialUserState, action);
+
+    expect(action.type).toEqual('user/get-profile/fulfilled');
+    expect(action.payload).toEqual(fakePayload);
+    expect(action.meta.requestId).toEqual(fakeRequestId);
+    expect(action.meta.arg).toEqual(undefined);
+
+    expect(state).toEqual({ ...intialUserState, profile: fakePayload });
+    expect(state.isProfileLoading).toBeFalsy;
+    expect(state.errorMessage).toBeNull;
+  });
+
+  it('Should FAIL to GET user profile', () => {
+    const fakePayload = null;
+
+    const action = getProfile.rejected(fakePayload, fakeRequestId);
+    const state = userReducer(intialUserState, action);
+
+    expect(action.type).toEqual('user/get-profile/rejected');
+    expect(action.payload).toEqual(undefined);
+    expect(action.meta.requestId).toEqual(fakeRequestId);
+    expect(action.meta.arg).toEqual(undefined);
+    expect(action.error).toEqual({ message: 'Rejected' });
+
+    expect(state).toEqual({
+      ...intialUserState,
+      profile: null,
+      errorMessage: 'Rejected',
+    });
+    expect(state.isProfileLoading).toBeFalsy;
+  });
+});
