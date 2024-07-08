d73692debd22ca88173a2c4c71c46f4d079ca9ce
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-08 13:02:37 -1000

    Add login function to AuthRepository interface

--- src/core/domain/repositories/AuthRepository.ts
+++ src/core/domain/repositories/AuthRepository.ts
@@ -1,6 +1,8 @@
-import { CheckAccountRequest } from '../../adapters/requests';
+import { CheckAccountRequest, LoginRequest } from '../../adapters/requests';
 import { ConfirmAccount } from '../entities/ConfirmAccount';
+import { User } from '../entities/User';
 
 export interface AuthRepository {
   checkAccount(req: CheckAccountRequest): Promise<ConfirmAccount>;
+  login(req: LoginRequest): Promise<User>;
 }
