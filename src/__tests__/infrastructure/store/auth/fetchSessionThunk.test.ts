dc5008c9ab25be8da382cfaf0e564603ef4507e2
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-11 18:23:55 -1000

    Add tests to FetchSessionThunk

--- src/__tests__/infrastructure/store/auth/fetchSessionThunk.test.ts
+++ src/__tests__/infrastructure/store/auth/fetchSessionThunk.test.ts
@@ -1,11 +1,11 @@
-import { validLoginResponse } from '../../../__mocks__/auth.mocks';
-import { fakeRequestId } from '../../../__mocks__/request.mocks';
+import { validLoginResponse } from '../../../../__mocks__/auth.mocks';
+import { fakeRequestId } from '../../../../__mocks__/request.mocks';
 import authReducer, {
   intialAuthState,
-} from '../../../infracstructure/store/reducers/auth';
-import { fetchSession } from '../../../core/domain';
+} from '../../../../infracstructure/store/reducers/auth';
+import { fetchSessionThunk } from '../../../../infracstructure/store/thunks/auth/FetchSessionThunk';
 
-jest.mock('../../../infracstructure/config/axios', () => ({
+jest.mock('../../../../infracstructure/config/axios', () => ({
   api: {
     baseUrl: 'http://localhost:3000',
   },
@@ -21,9 +21,9 @@ describe('Fetch session state test', () => {
   it('Should SUCCEED to fetch session user', () => {
     const fakePayload = validLoginResponse;
 
-    const action = fetchSession.fulfilled(fakePayload, fakeRequestId);
+    const action = fetchSessionThunk.fulfilled(fakePayload, fakeRequestId);
     const state = authReducer(intialAuthState, action);
-    expect(action.type).toEqual('settings/fetchSession/fulfilled');
+    expect(action.type).toEqual('settings/fetch-session/fulfilled');
     expect(action.payload).toEqual(fakePayload);
     expect(action.meta.requestId).toEqual(fakeRequestId);
     expect(action.meta.arg).toEqual(undefined);
@@ -34,10 +34,10 @@ describe('Fetch session state test', () => {
   it('Should FAIL to fetch session user', () => {
     const fakePayload = null;
 
-    const action = fetchSession.rejected(fakePayload, fakeRequestId);
+    const action = fetchSessionThunk.rejected(fakePayload, fakeRequestId);
     const state = authReducer(intialAuthState, action);
 
-    expect(action.type).toEqual('settings/fetchSession/rejected');
+    expect(action.type).toEqual('settings/fetch-session/rejected');
     expect(action.payload).toEqual(undefined);
     expect(action.meta.requestId).toEqual(fakeRequestId);
     expect(action.meta.arg).toEqual(undefined);
