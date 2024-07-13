528e83ef02962b3a6c64c2a687b9d2ef9494fc2e
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-12 15:58:40 -1000

    Add ForgotPasswordMapper

--- src/core/adapters/real/mappers/index.ts
+++ src/core/adapters/real/mappers/index.ts
@@ -2,3 +2,5 @@ export * from './ConfirmRegisterMapper';
 export * from './ConfirmLoginMapper';
 export * from './ConfirmAccountMapper';
 export * from './ConfirmLogoutMapper';
+export * from './ConfirmLoginMapper';
+export * from './ConfirmForgotPasswordMapper';
