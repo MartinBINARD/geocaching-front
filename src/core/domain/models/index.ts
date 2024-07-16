f5c48e156537522ec529e77430ffd48ba7eedb1b
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-16 12:32:41 -1000

    Add FetchCircuitsListUseCase class

--- src/core/domain/models/index.ts
+++ src/core/domain/models/index.ts
@@ -1,3 +1,4 @@
 export * from './Mapper';
 export * from './AuthErrors';
 export * from './ResultHandling';
+export * from './CircuitsErrors';
---
+++ src/core/usecases/circuits/FetchCircuitsListUseCase.ts
@@ -0,0 +1,24 @@
+import { CircuitsRepository } from '../../domain/repositories/CircuitsRepository';
+import { CircuitsList } from '../../domain/entities/Circuits';
+import { CircuitsErrors, ErrorOr, Result } from '../../domain/models';
+
+type Response = ErrorOr<CircuitsList>;
+
+export class FetchCircuitsListUseCase {
+  constructor(private circuitsRepository: CircuitsRepository) {}
+
+  public async execute(): Promise<Response> {
+    try {
+      const result = await this.circuitsRepository.fetchCircuitsList();
+
+      return Result.ok(result);
+    } catch (error) {
+      return Result.fail(
+        CircuitsErrors.FetchCircuitsLisError({
+          type: 'FETCH_CIRCUITS_LIST_ERROR',
+          details: error,
+        })
+      );
+    }
+  }
+}
