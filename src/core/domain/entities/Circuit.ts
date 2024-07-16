6565262b8b175bb28209cc539ced8cd0088fa7a6
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-16 11:03:16 -1000

    Add Circuit to entities/

---
+++ src/core/domain/entities/Circuit.ts
@@ -0,0 +1,21 @@
+export interface Circuit {
+  id_circuit: number;
+  city: string;
+  description: string;
+  difficulty: number;
+  distance: number;
+  duration: string;
+  introduction: string;
+  latitude: number;
+  longitude: number;
+  maintenance: boolean;
+  mobility: string[];
+  name: string;
+  parking_address: string;
+  region: string;
+  state: string;
+  terrain: number;
+  theme: string;
+  url_image: string;
+  url_reward: string;
+}
--- src/core/domain/entities/circuit.d.ts
+++ src/core/domain/entities/circuit.d.ts
@@ -1,24 +1,4 @@
-export interface Circuit {
-  id_circuit: number;
-  city: string;
-  description: string;
-  difficulty: number;
-  distance: number;
-  duration: string;
-  introduction: string;
-  latitude: number;
-  longitude: number;
-  maintenance: boolean;
-  mobility: string[];
-  name: string;
-  parking_address: string;
-  region: string;
-  state: string;
-  terrain: number;
-  theme: string;
-  url_image: string;
-  url_reward: string;
-}
+import { Circuit } from './Circuit';
 
 export interface CircuitPathStep {
   id: number;
