fdf240ef86b6f36b8ffe47358f063c6c2b852c00
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-16 18:08:44 -1000

    Add fetchCircuit to CircuitRepository

--- src/core/domain/repositories/CircuitsRepository.ts
+++ src/core/domain/repositories/CircuitsRepository.ts
@@ -1,5 +1,7 @@
+import { FetchCircuitRequest } from '../../adapters/requests/circuits/FetchCircuitRequest';
 import { Circuit } from '../entities/Circuits';
 
 export interface CircuitsRepository {
   fetchCircuitsList(): Promise<Circuit[]>;
+  fetchCircuit(req: FetchCircuitRequest): Promise<Circuit>;
 }
