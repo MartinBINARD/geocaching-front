36b9f48e645818ec5b8b9dee67de19956d2f4df2
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-16 18:29:17 -1000

    Add fetchCircuit prototype to RLCircuitsRepository class

--- src/core/adapters/real/mappers/index.ts
+++ src/core/adapters/real/mappers/index.ts
@@ -5,3 +5,6 @@ export * from './auth/ConfirmLogoutMapper';
 export * from './auth/ConfirmLoginMapper';
 export * from './auth/ConfirmForgotPasswordMapper';
 export * from './auth/ConfirmUpdatePasswordMapper';
+
+export * from './circuits/FetchCircuitsListMapper';
+export * from './circuits/FetchCircuitMapper';
--- src/core/adapters/real/repositories/RLCircuitsRepository.ts
+++ src/core/adapters/real/repositories/RLCircuitsRepository.ts
@@ -1,17 +1,25 @@
 import { AxiosInstance } from 'axios';
 import { CircuitsRepository } from '../../../domain/repositories/CircuitsRepository';
-import { CircuitsList } from '../../../domain/entities/Circuits';
-import { FetchCircuitsListMapper } from '../mappers/circuits/FetchCircuitsListMapper';
+import { FetchCircuitRequest } from '../../requests/circuits/FetchCircuitRequest';
+import { Circuit, CircuitsList } from '../../../domain/entities/Circuits';
+import { FetchCircuitsListMapper, FetchCircuitMapper } from '../mappers';
 
 export class RLCircuitsRepository implements CircuitsRepository {
   constructor(
     private httpClient: AxiosInstance,
-    private fetchCircuitsListMapper: FetchCircuitsListMapper
+    private fetchCircuitsListMapper: FetchCircuitsListMapper,
+    private fetchCircuitMapper: FetchCircuitMapper
   ) {}
 
   async fetchCircuitsList(): Promise<CircuitsList> {
     const result = await this.httpClient.get('circuits');
 
     return this.fetchCircuitsListMapper.toDomain(result.data);
+  }
+
+  async fetchCircuit(req: FetchCircuitRequest): Promise<Circuit> {
+    const result = await this.httpClient.get(`circuits/${req}`);
+
+    return this.fetchCircuitMapper.toDomain(result.data);
   }
 }
