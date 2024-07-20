458e0a9bd78375a8267236f060ae84c7adc46a76
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-19 18:05:17 -1000

    Remove useless storeCircuitQuizUseCase

--- src/core/usecases/circuits/storeCircuitQuizUseCase.ts
+++
@@ -1,6 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-import { CircuitPathStep } from '../../domain/entities/circuit';
-
-export const storeCircuitQuiz = createAction<CircuitPathStep[]>(
-  'circuits/store-circuit-quiz'
-);
