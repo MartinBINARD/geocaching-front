412d3e922aeeda6b354833a33cc7d05478e915a1
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-23 12:25:59 -1000

    Add updateProfile to UserErrors model

--- src/core/domain/models/UserErrors.ts
+++ src/core/domain/models/UserErrors.ts
@@ -8,4 +8,21 @@ export const UserErrors = {
       message: e.details.response?.data?.error,
     };
   },
+  updateProfileError(e: DomainErrorDTO): DomainErrorDTO {
+    const errorData = e.details.response?.data;
+
+    let errorMessage;
+
+    if (errorData?.message) {
+      errorMessage = errorData.message;
+    } else {
+      errorMessage = errorData.error;
+    }
+
+    return {
+      type: e.type,
+      details: e.details,
+      message: errorMessage,
+    };
+  },
 };
