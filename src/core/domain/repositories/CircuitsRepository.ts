ab1a77b247362cfffac6c9c4b41e427295310857
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-22 14:44:00 -1000

    Add CircuitQuizList to CircuitRespostiroty

--- src/core/domain/repositories/CircuitsRepository.ts
+++ src/core/domain/repositories/CircuitsRepository.ts
@@ -3,11 +3,11 @@ import {
   FilterCircuitListRequest,
   GetCircuitQuizRequest,
 } from '../../adapters/requests';
-import { Circuit, CircuitQuiz, CircuitsList } from '../entities';
+import { Circuit, CircuitQuizList, CircuitsList } from '../entities';
 
 export interface CircuitsRepository {
   fetchCircuitsList(): Promise<CircuitsList>;
   fetchCircuit(req: FetchCircuitRequest): Promise<Circuit>;
   filterCircuitsList(req: FilterCircuitListRequest): Promise<CircuitsList>;
-  getCircuitQuiz(req: GetCircuitQuizRequest): Promise<CircuitQuiz>;
+  getCircuitQuiz(req: GetCircuitQuizRequest): Promise<CircuitQuizList>;
 }
