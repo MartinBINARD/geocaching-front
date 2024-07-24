df5fb6dedb31116d36dd7a2a54b9d2e0fc00198f
MartinBINARD <martin.binard.dev@gmail.com>
2024-07-23 15:48:55 -1000

    Add deleteProfile to UserRepository

--- src/core/domain/repositories/UserRepository.ts
+++ src/core/domain/repositories/UserRepository.ts
@@ -1,7 +1,8 @@
 import { UpdateProfileRequest } from '../../adapters/requests/user/UpdateProfileRequest';
-import { Profile } from '../entities';
+import { ConfirmDeleteProfile, Profile } from '../entities';
 
 export interface UserRespository {
   getProfile(): Promise<Profile>;
   updateProfile(req: UpdateProfileRequest): Promise<Profile>;
+  deleteProfile(): Promise<ConfirmDeleteProfile>;
 }
